9a2c6f9c42c6cdb73ce9db8560eecdd29db2fdd2
Artem Agvanian <artem_agvanian@brown.edu>
2023-04-18 17:55:19 -0400

    feat: relaxation results

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@ input=$1
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/local/projects/cplex/CPLEX_Studio221/cplex/bin/x86-64_linux
 
 # add the solver jar to the classpath and run
-java -cp /local/projects/cplex/CPLEX_Studio221/cplex/lib/cplex.jar:src solver.ip.Main $input
+java -cp /local/projects/cplex/CPLEX_Studio221/cplex/lib/cplex.jar:src solver.ls.Main $input
