b9940fb83890275230e8c443cba41f46e92f682f
Artem Agvanian <artemagvanian@icloud.com>
2023-05-09 19:21:02 -0400

    feat: update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,4 +5,10 @@
 ########################################
 
 # Update this file with instructions on how to compile your code
-javac -classpath /local/projects/cplex/CPLEX_Studio221/cplex/lib/cplex.jar ./src/solver/ls/*.java ./src/solver/ls/data/*.java ./src/solver/ls/instances/*.java ./src/solver/ls/interchanges/*.java ./src/solver/ls/utils/*.java
+javac -classpath /local/projects/cplex/CPLEX_Studio221/cplex/lib/cplex.jar \
+    ./src/solver/ls/*.java \
+    ./src/solver/ls/data/*.java \
+    ./src/solver/ls/incremental/*.java \
+    ./src/solver/ls/instances/*.java \
+    ./src/solver/ls/interchanges/*.java \
+    ./src/solver/ls/utils/*.java
